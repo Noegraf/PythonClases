3ffe867aca66ef85f77cb0dbf299cc750b05a54a
Noe Valle <nouvalle.graf@gmail.com>
2022-08-10 04:27:18 -0300

    ej4

---
+++ ej4.py
@@ -0,0 +1,21 @@
+nombre = "Noelia"
+apellido = "Valle"
+edad = "33"
+email = "nouvalle.graf@gmail.com"
+telefono = 3412012364
+casado = False
+con_hijos = True
+lista_amigos = ["José", "Mati", "Lu", "Cos", "Pia", "Jotita", "Nico", "Fran", "Anton", "Mari", "Fede", "jony", "Seba"]
+dic_pelis = {
+    "peli1":"Totoro",
+    "peli2":"El viaje de Chihiro",
+    "peli3":"Zoolander"
+}
+
+datos_de_contacto = ["Nombre: "+nombre, "Apellido: "+apellido, "Edad: "+edad, "Email: "+email, "Teléfono: "+telefono, "¿Estas casada?: "+casado, "¿Tenes hijos?: "+con_hijos, "Lista de amigos: "+lista_amigos, "Peliculas que viste: "+dic_pelis]
+
+
+print(datos_de_contacto)
+
+
+
